7e78a62314f53e57c3b0fb8218d8892bc5222f59
Anthony Torres <atorres@g2crowd.com>
2019-01-21 20:39:43 -0600

    Set up for deployment

--- my_website/settings.py
+++ my_website/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,3 +119,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
