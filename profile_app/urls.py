8e6c6d66704830ad2557f58aa90f4cf0b8454c9b
Anthony P. Torres <anthony_torres01@yahoo.com>
2019-12-07 20:56:42 -0800

    Changed views.py

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import HomePageView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', HomePageView.as_view(), name='home'),
 ]
